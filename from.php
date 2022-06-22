762f9919176fbc9ddf2724b9d745358c68611073
Antonio <107107147+Ironman945@users.noreply.github.com>
2022-06-22 09:55:55 +0530

    Update from.php

--- from.php
+++ from.php
@@ -11,5 +11,5 @@
         $headers = "From $email_from \r\n";
         $headers = "Reply-To  $email \r\n";
         mail($to,$email_subject,$email_body,$headers);
-        header("Location: from.html");
+        header("Location: index.html");
 ?>
